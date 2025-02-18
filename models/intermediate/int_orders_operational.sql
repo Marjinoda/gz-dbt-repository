36c8d09dcbc53620b8d049ecb932e004b2633d95
Vincent Giner <bibabeloula62@gmail.com>
2025-02-18 14:12:26 +0000

    Operational Margin

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,12 @@
+SELECT 
+    orders_id, 
+    date_date, 
+    ROUND(margin + shipping_fee - logcost - ship_cost,2) AS operational_margin,
+    revenue,
+    quantity,
+    margin,
+    purchase_cost
+FROM {{ ref('int_orders_margin') }}
+JOIN {{ ref('stg_raw__ship') }}
+USING(orders_id)
+ORDER BY orders_id DESC
